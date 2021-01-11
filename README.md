ad8c0aecf728fb74413e77749513d3d0c8ec73cf
Nobert Ngungu <n.ngungu@carepay.co.ke>
2021-01-11 20:14:15 +0300

    aligning master and branch

--- README.md
+++ README.md
@@ -1,2 +1,56 @@
-# Disaster-Response-Project--Schoolwork
-A ML powered app for Classifying Disaster response messages
+# Disaster-Response-Project-Schoolwork
+This is a Machine Learning(ML) powered web app for classifying disaster response messages. It contains a web interface where a user can input a message and classify a message into several categories thereby reducing the time taken to manually label messages into categories; The model is trained specifically to classify disaster related messages into categories such as clothing, food, death, hospitals etc
+
+## Table of Contents
+1. [Project Motivation](#motivation)
+2. [Installations](#installations)
+3. [File Overview](#overview)
+4. [Instructions](#instructions)
+5. [Acknowledgements](#acknowledgements)
+
+## <a id="motivation"/> Project Motivation
+The motivation of this project was to build a machine learning based web app to help categorize disaster related messages using python's Machine Learning library scikit-learn and natural languae processing library nltk. I was also looking to put in practice on building ETL pipelines and in particular transforming and cleaning data to be ready to be used in training a model.
+
+## <a id="installations"/> Installations
+This project is mainly written in Python3 and the frontend utilizes HTML. The project requires the following packages to be installed:
+- pandas
+- numpy
+- sys
+- sqlalchemy
+- re
+- pickle
+- nltk
+- sklearn
+- plotly
+- json
+- joblib
+- flask
+
+##  <a id="overview"/> File Overview
+Files and folders included in this project include:
+- app: This folder contains:
+    - templates folder : the HTML files required to render the web app
+    - `run.py`: a python file which starts the app
+- data: This folder contains:
+    - disaster_messages.csv file : sample messages datasets to be used for model training
+    - disaster_categories.csv file: sample categories of the messages to be used for model training
+    - process_data.py : a python file that reads in the messages and categories csv files, merges them together, cleans/transforms the merged dataframe  and then creates a SQLite database with the cleaned dataframe as a table
+- models: This folder contains
+ - trian_classifier.py : a python file that loads the SQLite table produced by process.py and uses this data to train a ML model as well as evaluate the performance of the trianed model. The trained model is then saved as a pickle file to be used for predicting categories of new messages
+ NB: The pickle file is omitted from this repo because of its large file size but upon training the model locally  the pickle file is ave under this folder.
+
+## <a id="instructions"/> Instructions
+1. Run the following commands in the project's root directory to set up your database and model.
+
+    - To run ETL pipeline that cleans data and stores in database
+        `python3 data/process_data.py data/disaster_messages.csv data/disaster_categories.csv data/disaster_response.db`
+    - To run ML pipeline that trains classifier and saves
+        `python3 models/train_classifier.py data/disaster_response.db models/classifier.pkl`
+
+2. Run the following command in the app's directory to run your web app.
+    `python3 run.py`
+
+3. Go to http://0.0.0.0:3001/
+
+## <a id="acknowledgements"/> Licenses and acknowledgements
+This project was worked on as part of the Data Engineering module of the Udacity Data Scientist Nanodegree. The datasets are from Figure Eight who have collaborated with Udacity to provide data to be used to complete this ML project. Code templates were also provided by [Udacity](https://www.udacity.com/) .
---
+++ models/train_classifier.py
@@ -0,0 +1,193 @@
+import sys
+import pandas as pd
+import numpy as np
+from sqlalchemy import create_engine
+import re
+import pickle
+import nltk
+
+nltk.download('punkt')
+nltk.download('stopwords')
+
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+from nltk.stem.porter import PorterStemmer
+
+from sklearn.pipeline import Pipeline
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.multioutput import MultiOutputClassifier
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, make_scorer, confusion_matrix
+from sklearn.model_selection import GridSearchCV
+
+
+def load_data(database_filepath):
+    """
+    loads data into a dataframe from an sqllite database and splits the dataframe into messages df and the categories df
+
+    Parameters:
+        database_filepath: the filepath to the sqllite database
+    Returns:
+        X: a dataframe with the messages column
+        Y: a dataframe with only the categories columns
+        category_names: a list of all category column names
+
+    """
+     # Load data from database
+    engine = create_engine('sqlite:///' + database_filepath)
+    df = pd.read_sql("SELECT * FROM messages", engine)
+
+    # Create X and Y datasets
+    X = df['message']
+    Y = df.drop(df.columns[0:4], axis = 1)
+
+    # Create list containing all category names
+    category_names = list(Y.columns.values)
+
+    return X, Y, category_names
+
+
+def tokenize(text):
+    """
+    Normalize, tokenize and stemming text string
+
+    Parameters:
+        text: a string containing a sentence to be proceesed
+
+    Returns:
+        stemmed: list of strings containing normalized and stemmed word tokens
+
+    """
+    # Convert text to lowercase and remove punctuation
+    text = re.sub(r"[^a-zA-Z0-9]", " ", text)
+    text = text.lower()
+
+    # Tokenize words
+    tokens = word_tokenize(text)
+
+    # Stem word tokens and remove stop words
+    ps = PorterStemmer()
+    stop_words = stopwords.words("english")
+
+    stemmed = [ps.stem(word) for word in tokens if word not in stop_words]
+
+    return stemmed
+
+def build_model():
+    """
+    Build a machine learning(ML) pipeline
+
+    Parameters:
+        None
+
+    Returns:
+        cv: Gridsearchcv object that transforms the data, creates a
+        model and gets the best model parameters.
+
+    """
+    # Create a pipeline
+    pipeline = Pipeline([
+        ('text_pipeline', Pipeline([
+            ('vect', CountVectorizer(tokenizer = tokenize)),
+            ('tfidf', TfidfTransformer())
+        ])),
+        ('clf', MultiOutputClassifier(RandomForestClassifier()))
+    ])
+
+    # Create parameters dictionary
+    parameters = {
+        'text_pipeline__vect__ngram_range': ((1, 1), (1, 2)),
+        'text_pipeline__vect__max_df': (0.5, 0.75, 1.0),
+        'text_pipeline__vect__max_features': (None, 5000, 10000),
+        'text_pipeline__tfidf__use_idf': (True, False),
+        'clf__estimator__n_estimators': [50, 100, 200],
+        'clf__estimator__min_samples_split': [2, 3, 4],
+    }
+
+    # Create grid search object
+    cv = GridSearchCV(pipeline, param_grid = parameters, verbose = 10)
+
+    return cv
+
+
+def evaluate_model(model, X_test, Y_test, category_names):
+    """
+    Gets testing dataset accuracy, precision, recall and F1 score for fitted model
+
+    Parameters:
+        model: Fitted model object.
+        X_test: dataframe containing test features dataset.
+        Y_test: dataframe containing test target dataset.
+        category_names: list of the category names.
+
+    Returns:
+        None
+
+    """
+    # get predicted labels for test dataset
+    Y_pred = model.predict(X_test)
+
+    #create an empty metrics list
+    metrics_list = []
+
+    # get evaluation metrics for each of the labels
+    for i in range(len(category_names)):
+        # convert Y_test to an array
+        Y_test = np.array(Y_test)
+
+        accuracy = accuracy_score(Y_test[:, i], Y_pred[:, i])
+        precision = precision_score(Y_test[:, i],Y_pred[:, i], average=None)
+        recall = recall_score(Y_test[:, i], Y_pred[:, i], average=None)
+        f1 = f1_score(Y_test[:, i], Y_pred[:, i], average=None)
+
+        metrics_list.append([accuracy, precision, recall, f1])
+
+    # Create dataframe containing metrics and print results
+    metrics = np.array(metrics_list)
+    metrics_df = pd.DataFrame(data = metrics, columns = ['Accuracy', 'Precision', 'Recall', 'F1'], index = category_names, )
+    print(metrics_df)
+
+def save_model(model, model_filepath):
+    """
+    Save the fitted model
+
+    Parameters:
+        model: fitted model object.
+        model_filepath: filepath string for where fitted model should be saved
+
+    Returns:
+        None
+    """
+    pickle.dump(model.best_estimator_, open(model_filepath, 'wb'))
+
+
+def main():
+    if len(sys.argv) == 3:
+        database_filepath, model_filepath = sys.argv[1:]
+        print('Loading data...\n    DATABASE: {}'.format(database_filepath))
+        X, Y, category_names = load_data(database_filepath)
+        X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.3)
+
+        print('Building model...')
+        model = build_model()
+
+        print('Training model...')
+        model.fit(X_train, Y_train)
+
+        print('Evaluating model...')
+        evaluate_model(model, X_test, Y_test, category_names)
+
+        print('Saving model...\n    MODEL: {}'.format(model_filepath))
+        save_model(model, model_filepath)
+
+        print('Trained model saved!')
+    else:
+        print('Please provide the filepath of the disaster messages database '\
+              'as the first argument and the filepath of the pickle file to '\
+              'save the model to as the second argument. \n\nExample: python '\
+              'train_classifier.py ../data/DisasterResponse.db classifier.pkl')
+
+
+if __name__ == '__main__':
+    main()
